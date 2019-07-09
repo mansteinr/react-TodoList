9638dd70c6d2940a2099bcd5cf78414f53c28297
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-07-09 18:25:15 +0800

    删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,7 +6,7 @@ class TodoList extends Component {
     super(props)
     this.state = {
       // input框中的值
-      inputValue: 'hello',
+      inputValue: '',
       // 数组中的值
       list: []
     }
@@ -15,12 +15,21 @@ class TodoList extends Component {
     return(
       <Fragment> 
         <input
+          // 原生DOMchange事件绑定为onchange
+          // 绑定chang事件, 需要绑定this，否则this指向不是该组件
           onChange={this.handleInputChange.bind(this)}
           value={this.state.inputValue}/>
-        <button>提交</button>
+          {/* 绑定提交事件 */}
+        <button onClick={this.handleBtnClick.bind(this)}>提交</button>
         <ul>
-          <li>音乐</li>
-          <li>music</li>
+          {
+            this.state.list.map((v, k) => {
+              return <li 
+                key={ k }
+                onClick={this.handleDelete.bind(this, k)}
+              >{ v }</li>
+            })
+          }
         </ul>
       </Fragment>
     )
@@ -28,6 +37,26 @@ class TodoList extends Component {
   handleInputChange(e) {
     this.setState({
       inputValue: e.target.value
+    })
+  }
+
+  handleBtnClick() {
+    this.setState({
+      // 点击的时候 list数组内容新增 输入项
+      list: [...this.state.list, this.state.inputValue],
+      // 情况inputValue
+      inputValue: ''
+    })
+  }
+
+  // 删除事件
+  handleDelete(index) {
+    let list = [...this.state.list]
+    list.splice(index, 1)
+    //1. 不希望直接修改state里面的数据 故先拷贝出来
+    //2. splice 删除后返回的是被删除的项，并改变原数组
+    this.setState({
+      list: list
     })
   }
 }

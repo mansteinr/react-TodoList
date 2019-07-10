47cfa49b8139de0380c1406f881a6a48377cee69
MINIVISION\zhoumingye <zhoumingye@minivision.cn>
2019-07-10 17:43:57 +0800

    强校验和默认值

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends React.Component{
   constructor(props) {
@@ -7,17 +8,31 @@ class TodoItem extends React.Component{
   }
 
   render() {
-    const { content } = this.props
+    const { content, text } = this.props
     // this.props.content 接受父组件传递过来的信息
     // dangerouslySetInnerHTML={{__html: this.props.content}} 渲染html片段
     // return <div dangerouslySetInnerHTML={{__html: this.props.content}}></div>
-    return <div onClick={this.handleClick}>{ content }</div>
+    return <div onClick={this.handleClick}>{ text } - { content }</div>
   }
   
   handleClick() {
     // 调用父组件的方法 并把对应的索引传给父组件
     this.props.deleteItme(this.props.index)
   }
+}
+
+// 对父组件的传值 进行强校验
+TodoItem.propTypes = {
+  content: PropTypes.string,
+  deleteItme: PropTypes.func,
+  index: PropTypes.number,
+  // 必须要传字符串text 上面的不是必传
+  text: PropTypes.string.isRequired
+}
+
+// 对父组件的传值 进行默认值赋值
+TodoItem.defaultProps = {
+  text: 'hello world'
 }
 
 export default TodoItem
